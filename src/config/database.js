20791af30046d97cd038a9cd6f1fbf304f7b74b6
praful-koli <prafulkoli852@gmail.com>
2026-05-11 14:28:49 +0530

    user model schema

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,15 @@
-const mongoose = require('mongoose')
-const config = require('./config.js')
+const mongoose = require("mongoose");
+const config = require("./config.js");
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(config.DB_URL)
-        console.log('MongoDB connected successfully')
-    } catch (error) {
-        console.error('MongoDB connection error:', error)
-    }
-}
+  try {
+    await mongoose
+      .connect(config.DB_URL)
+      .then(() => console.log("MongoDB connected successfully"));
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
---
+++ src/models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: [true, "Email already exists"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+      trim: true,
+      lowercase: true,
+    },
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+      trim: true,
+      minLength: [6, "Password must be at least 6 characters long"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+  } else {
+    return next();
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
